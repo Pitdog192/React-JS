1e9fa2faf280b7a28b39a30ce13c8761c0aa1482
Santiago <arroqui192@gmail.com>
2022-07-21 14:17:22 -0300

    21/7

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,7 +1,10 @@
+import React from 'react';
+
 const ItemListContainer = ({greeting}) => {
     return (
         <div className="contenedorPrincipal">
             <h1 className="titulo"> {greeting} </h1>
+
         </div> 
     )
 }
